4957ce3646a252181e31225ebaac49c8ac4fd215
Danish Zahid <danishst.johns@gmail.com>
2022-10-05 11:51:36 +0530

    Part4 Done: area.html

---
+++ area.js
@@ -0,0 +1,12 @@
+const base = document.querySelector(".base");
+const height = document.querySelector(".height");
+const areaBtn = document.querySelector("#area-btn");
+const output = document.querySelector(".output");
+
+
+areaBtn.addEventListener("click", function claculateArea(){
+    const a = Number(base.value); 
+    const b = Number(height.value);
+    const area = 0.5*a*b;
+    output.innerText = "Area calculated is "+area;
+})
